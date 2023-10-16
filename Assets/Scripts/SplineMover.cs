96824452a259295cfc93fe8f4b82c943c03bc3d3
Arthur Caumartin <artcaumart@gmail.com>
2023-10-16 11:48:25 +0200

    Spline Mover

--- Assets/Scripts/SplineMover.cs
+++ Assets/Scripts/SplineMover.cs
@@ -7,25 +7,14 @@ public class SplineMover : MonoBehaviour
 {
     [SerializeField] Spline _spline;
     [SerializeField] private float _speed;
-    [SerializeField] private float _distance;
+    private float _distance;
     Vector3 _newPosition;
-    Vector3 _lastFramePos;
 
     void Update()
     {
-
         _distance += Time.deltaTime * _speed;
         _newPosition = _spline.computePointWithLength(_distance);
-        // _distance = Mathf.InverseLerp(-1, 1, Mathf.Sin(Time.time * _speed));
-        // _newPosition = _spline.computePoint(_distance);
 
         transform.position = _newPosition;
-
-        if(transform.position == _lastFramePos)
-        {
-            
-        }
-
-        _lastFramePos = transform.position;
     }
 }
