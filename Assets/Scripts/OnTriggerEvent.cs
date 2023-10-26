29db95eb2148e8f4b660cd88fd1c3738cc0db304
Arthur Caumartin <artcaumart@gmail.com>
2023-10-26 23:39:53 +0200

    Add trigger event + spline mover SetSpeed fct

---
+++ Assets/Scripts/OnTriggerEvent.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class OnTriggerEvent : MonoBehaviour
+{
+    [SerializeField] private string _tagToTrigger;
+    [SerializeField] private UnityEvent _onTriggerEvent;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == _tagToTrigger)
+        {
+            _onTriggerEvent.Invoke();
+        }
+    }    
+}
