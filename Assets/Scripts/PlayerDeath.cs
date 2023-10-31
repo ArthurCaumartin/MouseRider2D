637b5fe5214826ea075d4a261fff27fa06e32dfc
Sybirk <115795460+Sybirk@users.noreply.github.com>
2023-10-31 15:14:28 +0100

    Commit Oscar

--- Assets/Scripts/PlayerDeath.cs
+++ Assets/Scripts/PlayerDeath.cs
@@ -1,15 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerDeath : MonoBehaviour
 {
+    public Shaking shaking;
+    
    private void OnTriggerEnter2D(Collider2D other) 
    {
+        
         if(other.CompareTag ("Obstacle"))
         {
             GameManager.instance.EndLevel();
             GetComponentInParent<SplineMover>()._isMoving  = false;
+            shaking.GetComponent<Shaking>();
+            shaking.start = true;
         }
    }
 }
---
+++ Assets/Scripts/Shaking.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using UnityEngine;
+
+public class Shaking : MonoBehaviour
+{
+    public bool start = false;
+    public float duration = 1.5f;
+    public AnimationCurve curve;
+    void Update()
+    {
+        if(start == true)
+        {
+            StartCoroutine(Shake());
+        }
+    }
+    IEnumerator Shake()
+    {
+        Vector3 startPosition = transform.position;
+        float elapsedTime = 0f;
+
+        while(elapsedTime < duration && duration > 0)
+        {
+            duration = duration - Time.deltaTime;
+            elapsedTime += Time.deltaTime;
+            transform.position = startPosition + Random.insideUnitSphere;
+            yield return null;
+        }
+        transform.position = startPosition;
+    }
+}
