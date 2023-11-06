99a1caf3bf8313a1927e344b4116309395026e04
Sybirk <115795460+Sybirk@users.noreply.github.com>
2023-11-06 11:25:50 +0100

    OscarCleanContainer

--- Assets/Scripts/PlayerDeath.cs
+++ Assets/Scripts/PlayerDeath.cs
@@ -2,23 +2,22 @@ using UnityEngine;
 using DG.Tweening;
 public class PlayerDeath : MonoBehaviour
 {
-    public Shaking shaking;
-    public Camera SizeCamera;
-    
+    public Shaking shaking; 
    private void OnTriggerEnter2D(Collider2D other) 
    {  
         if(other.CompareTag ("Obstacle"))
+        
         {
-            GameManager.instance.EndLevel();
-            GetComponentInParent<SplineMover>()._isMoving  = false;
-            shaking.GetComponent<Shaking>();
-            shaking.start = true;
-            new WaitForSeconds(3f);
-            CameraDezoom();
+          GameManager.instance.EndLevel();
+          GetComponentInParent<SplineMover>()._isMoving  = false;
+          shaking.GetComponent<Shaking>();
+          shaking.start = true;
+          new WaitForSeconds(3f);
+          CameraDezoom();
         }
    }
    public void CameraDezoom()
    {
-        SizeCamera.DOOrthoSize(9f, 3f);
+     Camera.main.DOOrthoSize(9f, 3f);
    }
 }
