5b766a79b0e4e1f5c94b70e8f3f932cbb0a737f6
Pix-Hell <119054517+Pix-Hell@users.noreply.github.com>
2023-11-06 10:52:20 +0100

    Food system V1 done

--- Assets/Scripts/FoodCounter.cs
+++ Assets/Scripts/FoodCounter.cs
@@ -5,6 +5,24 @@ using UnityEngine;
 public class FoodCounter : MonoBehaviour
 {
     public int foodCount;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Food"))
+        {
+            Destroy(other.gameObject);
+            foodCount++;
+
+            FoodCheck();
+        }
+    }
+
+    private void FoodCheck()
+    {
+        if (foodCount == 3)
+        {
+            Debug.Log("AllFood");
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
