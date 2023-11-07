8b174fe32e2e9c7efa1eceb7ce2e22431e97ed39
Sybirk <115795460+Sybirk@users.noreply.github.com>
2023-11-07 14:28:47 +0100

    Score and UI Update

--- Assets/Scripts/FoodCounter.cs
+++ Assets/Scripts/FoodCounter.cs
@@ -2,29 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
-
+using UnityEngine.UI;
+using TMPro;
+using System;
 public class FoodCounter : MonoBehaviour
 {
+    public TMP_Text scoreText;
     public AnimationCurve foodForm; 
     public int foodCount;
-
     public void FoodCheck(int foodToAdd)
     {
+        Debug.Log("Eat");
         foodCount += foodToAdd;
+        scoreText.text = foodCount.ToString() + "=";
         if (foodCount == 3)
         {
-            Debug.Log("AllFood");
         }
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
