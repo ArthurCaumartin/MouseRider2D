e2d6f93e657c2988d20491fa9001d89cd8202169
Sybirk <115795460+Sybirk@users.noreply.github.com>
2023-11-17 10:07:17 +0100

    UpdatePièce

--- Assets/Scripts/CanvasManager.cs
+++ Assets/Scripts/CanvasManager.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 public class CanvasManager : MonoBehaviour
 {
+    public FoodCounter foodcounter;
     public static CanvasManager instance;
     public List<Image> FoodImageList = new List<Image>();
     public List<Destroy_Food> FoodList = new List<Destroy_Food>();
@@ -21,9 +22,28 @@ public class CanvasManager : MonoBehaviour
             }
         }
         FoodImageList[index].color = Color.yellow;
+
+
     }
     void Awake()
     {
         instance = this;
+    }
+    private void Start() 
+    {
+        foodcounter.GetComponent<FoodCounter>();
+    }
+    private void Update() 
+    {
+        RaimbowMoney();
+    }
+    public void RaimbowMoney()
+    {
+        if(foodcounter.foodCount >= 3)
+        {
+            FoodImageList[0].color = Color.blue;
+            FoodImageList[1].color = Color.white;
+            FoodImageList[2].color = Color.red;
+        }
     }
 }
