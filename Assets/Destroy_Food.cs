e3ff52f7d2824e9b288b2c631320ff6e2ad93ccd
Pix-Hell <119054517+Pix-Hell@users.noreply.github.com>
2023-11-19 12:26:11 +0100

    Musique fonctionne/ Audio bonk, coin décalé/ time slow qui marche pas

--- Assets/Destroy_Food.cs
+++ Assets/Destroy_Food.cs
@@ -5,16 +5,20 @@ using DG.Tweening;
 
 public class Destroy_Food : MonoBehaviour
 {
+    [SerializeField] private AudioClip audioCoin = null;
     [SerializeField] float AnimationDuration;
     public AnimationCurve foodForm;
+    private AudioSource coin_Collect;
     private void OnTriggerEnter2D(Collider2D other)
     {
+        coin_Collect = GetComponent<AudioSource>();
         FoodCounter foodCounter = other.GetComponent<FoodCounter>();
         ParticleSystem particle = GetComponentInChildren<ParticleSystem>();
         particle.Play();
         CanvasManager.instance.ScoreUPdateFood(this);
         if (foodCounter != null)
         {
+            coin_Collect.PlayOneShot(audioCoin);
             foodCounter.FoodCheck(1);
             transform.DOScale(0f, AnimationDuration)
                 .SetEase(foodForm)
